184082e608ee25dcfb142906fd6d97ec489d1aa0
Dolya08 <74494396+Dolya08@users.noreply.github.com>
2023-08-22 06:58:05 +0300

    Все до конца 9-ого урока

--- app/src/main/java/tmp/dolya/kalmlang/GameLevels.java
+++ app/src/main/java/tmp/dolya/kalmlang/GameLevels.java
@@ -1,8 +1,11 @@
 package tmp.dolya.kalmlang;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -16,5 +19,30 @@ public class GameLevels extends AppCompatActivity {
         Window w = getWindow();
         w.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
+        Button btn_back = (Button)findViewById(R.id.btn_back);
+        btn_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    Intent intent = new Intent(GameLevels.this, MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                }catch (Exception e){
+
+                }
+            }
+        });
+
+    }
+    //  Системная кнопка назад
+    @Override
+    public void onBackPressed() {
+        try {
+            Intent intent = new Intent(GameLevels.this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        }catch (Exception e){
+
+        }
     }
 }
