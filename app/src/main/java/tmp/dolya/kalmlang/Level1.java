5694c810be0f6ec0fd3cf8bfd93831eee35f41bf
Dolya08 <74494396+Dolya08@users.noreply.github.com>
2023-08-28 03:57:20 +0300

    Нужно что-то сделать с кнопкой "?"

--- app/src/main/java/tmp/dolya/kalmlang/Level1.java
+++ app/src/main/java/tmp/dolya/kalmlang/Level1.java
@@ -2,12 +2,20 @@ package tmp.dolya.kalmlang;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 public class Level1 extends AppCompatActivity {
+
+    Dialog dialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +28,45 @@ public class Level1 extends AppCompatActivity {
 
         Window w = getWindow();
         w.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        dialog = new Dialog(this);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setContentView(R.layout.previewdialog);
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        dialog.setCancelable(true); //можно ли закрыть диалоговое окно кнопкой назад
+
+        TextView btnclose = (TextView)dialog.findViewById(R.id.btnclose);
+        btnclose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
+
+        TextView btnquest = (TextView)this.findViewById(R.id.btnquest);
+        btnquest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+                    dialog.setContentView(R.layout.previewdialog);
+                    dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                    dialog.setCancelable(true); //можно ли закрыть диалоговое окно кнопкой назад
+
+                    TextView btnclose = (TextView)dialog.findViewById(R.id.btnclose);
+                    btnclose.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            dialog.dismiss();
+                        }
+                    });
+                    dialog.show();
+                } catch (Exception e) {
+
+                }
+            }
+        });
+
+        dialog.show();
     }
 }
