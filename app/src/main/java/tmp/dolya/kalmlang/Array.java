e7eed02979ba12a945c41b92b4c02ffaccd05f62
Dolya <dolgan-goryaev@mail.ru>
2023-08-31 17:23:05 +0300

    Все до 27-ого урока

---
+++ app/src/main/java/tmp/dolya/kalmlang/Array.java
@@ -0,0 +1,38 @@
+package tmp.dolya.kalmlang;
+
+public class Array {
+    // Массив для первого уровня
+    final int[] images1 = {
+            R.drawable.apple,
+            R.drawable.cherry,
+            R.drawable.plum,
+            R.drawable.peach,
+            R.drawable.pear,
+            R.drawable.grape,
+            R.drawable.tomato,
+            R.drawable.cucmber,
+            R.drawable.cabbage,
+            R.drawable.carrot,
+            R.drawable.potato,
+            R.drawable.onion,
+            R.drawable.melon,
+            R.drawable.watermelon
+    };
+
+    final int[] texts1 = {
+            R.string.lvl1text0,
+            R.string.lvl1text1,
+            R.string.lvl1text2,
+            R.string.lvl1text3,
+            R.string.lvl1text4,
+            R.string.lvl1text5,
+            R.string.lvl1text6,
+            R.string.lvl1text7,
+            R.string.lvl1text8,
+            R.string.lvl1text9,
+            R.string.lvl1text10,
+            R.string.lvl1text11,
+            R.string.lvl1text12,
+            R.string.lvl1text13
+    };
+}
