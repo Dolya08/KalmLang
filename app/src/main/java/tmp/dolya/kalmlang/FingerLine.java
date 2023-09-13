ab21f562f8d2a5e956519f15430787d251c1e7a2
Dolya08 <74494396+Dolya08@users.noreply.github.com>
2023-09-13 07:04:08 +0300

    Доделать 3 ур

--- app/src/main/java/tmp/dolya/kalmlang/FingerLine.java
+++ app/src/main/java/tmp/dolya/kalmlang/FingerLine.java
@@ -15,6 +15,8 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 
+import java.util.Arrays;
+
 public class FingerLine extends View {
     private final Paint mPaint;
     Level3_2 level3_2 = new Level3_2();
@@ -23,8 +25,6 @@ public class FingerLine extends View {
     private float endX;
     private float endY;
     public Canvas mCanvas;
-    int[] pos = new int[2];
-    float[] x1, y1, x2, y2;
     ImageView item1, item2, item3, item4;
     TextView numb1, numb2, numb3, numb4;
 
@@ -32,64 +32,14 @@ public class FingerLine extends View {
         this(context, null);
         Bitmap mBitmap = Bitmap.createBitmap(level3_2.width, level3_2.height, Bitmap.Config.ARGB_8888);
         mCanvas = new Canvas(mBitmap);
-        item1 = (ImageView)findViewById(R.id.imgItem1);
-        item2 = (ImageView)findViewById(R.id.imgItem2);
-        item3 = (ImageView)findViewById(R.id.imgItem3);
-        item4 = (ImageView)findViewById(R.id.imgItem4);
-        numb1 = (TextView)findViewById(R.id.numbItem1);
-        numb2 = (TextView)findViewById(R.id.numbItem2);
-        numb3 = (TextView)findViewById(R.id.numbItem3);
-        numb4 = (TextView)findViewById(R.id.numbItem4);
-        x1[0] = pos[0];
-        y1[0] = pos[1];
-        x2[0] = x1[0] + item1.getWidth();
-        y2[0] = y1[0] + item1.getHeight();
-        Log.d("x1", String.valueOf(x1[0]));
-        Log.d("x2", String.valueOf(x2[0]));
-        Log.d("y1", String.valueOf(y1[0]));
-        Log.d("y2", String.valueOf(y2[0]));
-
-        item2.getLocationOnScreen(pos);
-        x1[1] = pos[0];
-        y1[1] = pos[1];
-        x2[1] = x1[1] + item2.getWidth();
-        y2[1] = y1[1] + item2.getHeight();
-
-        item3.getLocationOnScreen(pos);
-        x1[2] = pos[0];
-        y1[2] = pos[1];
-        x2[2] = x1[2] + item3.getWidth();
-        y2[2] = y1[2] + item3.getHeight();
-
-        item4.getLocationOnScreen(pos);
-        x1[3] = pos[0];
-        y1[3] = pos[1];
-        x2[3] = x1[3] + item4.getWidth();
-        y2[3] = y1[3] + item4.getHeight();
-
-        numb1.getLocationOnScreen(pos);
-        x1[0] = pos[0];
-        y1[0] = pos[1];
-        x2[0] = x1[0] + numb1.getWidth();
-        y2[0] = y1[0] + numb1.getHeight();
-
-        numb2.getLocationOnScreen(pos);
-        x1[1] = pos[0];
-        y1[1] = pos[1];
-        x2[1] = x1[1] + numb2.getWidth();
-        y2[1] = y1[1] + numb2.getHeight();
-
-        numb3.getLocationOnScreen(pos);
-        x1[2] = pos[0];
-        y1[2] = pos[1];
-        x2[2] = x1[2] + numb3.getWidth();
-        y2[2] = y1[2] + numb3.getHeight();
-
-        numb4.getLocationOnScreen(pos);
-        x1[3] = pos[0];
-        y1[3] = pos[1];
-        x2[3] = x1[3] + numb4.getWidth();
-        y2[3] = y1[3] + numb4.getHeight();
+        item1 = (ImageView)findViewById(level3_2.item1.getId());
+        item2 = (ImageView)findViewById(level3_2.item2.getId());
+        item3 = (ImageView)findViewById(level3_2.item3.getId());
+        item4 = (ImageView)findViewById(level3_2.item4.getId());
+        numb1 = (TextView)findViewById(level3_2.numb1.getId());
+        numb2 = (TextView)findViewById(level3_2.numb2.getId());
+        numb3 = (TextView)findViewById(level3_2.numb3.getId());
+        numb4 = (TextView)findViewById(level3_2.numb4.getId());
     }
 
     public FingerLine(Context context, AttributeSet attrs) {
@@ -113,18 +63,19 @@ public class FingerLine extends View {
             case MotionEvent.ACTION_DOWN:
                 startX = event.getX();
                 startY = event.getY();
-                if (x1[0] < startX && startX < x2[0] && y1[0] < startY && startY < y2[0]) {
-                    startX = x1[0] + (float)item1.getWidth() / 2;
-                    startY = y1[0] + item1.getHeight();
-                } else if (x1[1] < startX && startX < x2[1] && y1[1] < startY && startY < y2[1]) {
-                    startX = x1[1] + (float)item2.getWidth() / 2;
-                    startY = y1[1] + item2.getHeight();
-                } else if (x1[2] < startX && startX < x2[2] && y1[2] < startY && startY < y2[2]) {
-                    startX = x1[2] + (float)item3.getWidth() / 2;
-                    startY = y1[2] + item3.getHeight();
-                } else if (x1[3] < startX && startX < x2[3] && y1[3] < startY && startY < y2[3]) {
-                    startX = x1[3] + (float)item4.getWidth() / 2;
-                    startY = y1[3] + item4.getHeight();
+                Log.d("pos_x", Arrays.toString(level3_2.x1));
+                if (level3_2.x1[0] < startX && startX < level3_2.x2[0] && level3_2.y1[0] < startY && startY < level3_2.y2[0]) {
+                    startX = level3_2.x1[0] + (float)item1.getWidth() / 2;
+                    startY = level3_2.y1[0] + item1.getHeight();
+                } else if (level3_2.x1[1] < startX && startX < level3_2.x2[1] && level3_2.y1[1] < startY && startY < level3_2.y2[1]) {
+                    startX = level3_2.x1[1] + (float)item2.getWidth() / 2;
+                    startY = level3_2.y1[1] + item2.getHeight();
+                } else if (level3_2.x1[2] < startX && startX < level3_2.x2[2] && level3_2.y1[2] < startY && startY < level3_2.y2[2]) {
+                    startX = level3_2.x1[2] + (float)item3.getWidth() / 2;
+                    startY = level3_2.y1[2] + item3.getHeight();
+                } else if (level3_2.x1[3] < startX && startX < level3_2.x2[3] && level3_2.y1[3] < startY && startY < level3_2.y2[3]) {
+                    startX = level3_2.x1[3] + (float)item4.getWidth() / 2;
+                    startY = level3_2.y1[3] + item4.getHeight();
                 }
                 invalidate();
                 break;
@@ -136,18 +87,18 @@ public class FingerLine extends View {
             case MotionEvent.ACTION_UP:
                 endX = event.getX();
                 endY = event.getY();
-                if (x1[0] < endX && endX < x2[0] && y1[0] < endY && endY < y2[0]) {
-                    endX = x1[0] + (float)item1.getWidth() / 2;
-                    endY = y1[0];
-                } else if (x1[1] < endX && endX < x2[1] && y1[1] < endY && endY < y2[1]) {
-                    endX = x1[1] + (float)item2.getWidth() / 2;
-                    endY = y1[1];
-                } else if (x1[2] < endX && endX < x2[2] && y1[2] < endY && endY < y2[2]) {
-                    endX = x1[2] + (float)item3.getWidth() / 2;
-                    endY = y1[2];
-                } else if (x1[3] < endX && endX < x2[3] && y1[3] < endY && endY < y2[3]) {
-                    endX = x1[3] + (float)item4.getWidth() / 2;
-                    endY = y1[3];
+                if (level3_2.x1[0] < endX && endX < level3_2.x2[0] && level3_2.y1[0] < endY && endY < level3_2.y2[0]) {
+                    endX = level3_2.x1[0] + (float)item1.getWidth() / 2;
+                    endY = level3_2.y1[0];
+                } else if (level3_2.x1[1] < endX && endX < level3_2.x2[1] && level3_2.y1[1] < endY && endY < level3_2.y2[1]) {
+                    endX = level3_2.x1[1] + (float)item2.getWidth() / 2;
+                    endY = level3_2.y1[1];
+                } else if (level3_2.x1[2] < endX && endX < level3_2.x2[2] && level3_2.y1[2] < endY && endY < level3_2.y2[2]) {
+                    endX = level3_2.x1[2] + (float)item3.getWidth() / 2;
+                    endY = level3_2.y1[2];
+                } else if (level3_2.x1[3] < endX && endX < level3_2.x2[3] && level3_2.y1[3] < endY && endY < level3_2.y2[3]) {
+                    endX = level3_2.x1[3] + (float)item4.getWidth() / 2;
+                    endY = level3_2.y1[3];
                 }
                 mCanvas.drawLine(startX, startY, endX, endY, mPaint);
                 invalidate();
--- app/src/main/java/tmp/dolya/kalmlang/Level3_2.java
+++ app/src/main/java/tmp/dolya/kalmlang/Level3_2.java
@@ -13,12 +13,15 @@ import android.util.Log;
 import android.view.DragEvent;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewTreeObserver;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Arrays;
 
 public class Level3_2 extends AppCompatActivity {
     ImageView item1, item2, item3, item4;
@@ -32,14 +35,6 @@ public class Level3_2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.universallvl3_2);
-        item1 = (ImageView)findViewById(R.id.imgItem1);
-        item2 = (ImageView)findViewById(R.id.imgItem2);
-        item3 = (ImageView)findViewById(R.id.imgItem3);
-        item4 = (ImageView)findViewById(R.id.imgItem4);
-        numb1 = (TextView)findViewById(R.id.numbItem1);
-        numb2 = (TextView)findViewById(R.id.numbItem2);
-        numb3 = (TextView)findViewById(R.id.numbItem3);
-        numb4 = (TextView)findViewById(R.id.numbItem4);
         container1lvl = (LinearLayout)findViewById(R.id.container1lvl);
 
         Button btn_back = (Button)findViewById(R.id.btn_back_lvl);
@@ -59,12 +54,68 @@ public class Level3_2 extends AppCompatActivity {
 
         Button btn_next = (Button)findViewById(R.id.btn_next_lvl);
         btn_next.setVisibility(View.INVISIBLE);
-        onPostCreate();
-
     }
-    @SuppressLint("ClickableViewAccessibility")
-    public void onPostCreate() {
+    @Override
+    public void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        item1 = (ImageView)findViewById(R.id.imgItem1);
+        item2 = (ImageView)findViewById(R.id.imgItem2);
+        item3 = (ImageView)findViewById(R.id.imgItem3);
+        item4 = (ImageView)findViewById(R.id.imgItem4);
+        numb1 = (TextView)findViewById(R.id.numbItem1);
+        numb2 = (TextView)findViewById(R.id.numbItem2);
+        numb3 = (TextView)findViewById(R.id.numbItem3);
+        numb4 = (TextView)findViewById(R.id.numbItem4);
+
         item1.getLocationOnScreen(pos);
+        Log.d("pos_x", String.valueOf(item1.getX()));
+        Log.d("pos_y", String.valueOf(item1.getY()));
+        x1[0] = (float)pos[0];
+        y1[0] = (float)pos[1];
+        x2[0] = x1[0] + (float)item1.getWidth();
+        y2[0] = y1[0] + (float)item1.getHeight();
+
+        item2.getLocationOnScreen(pos);
+        x1[1] = (float)pos[0];
+        y1[1] = (float)pos[1];
+        x2[1] = x1[1] + (float)item2.getWidth();
+        y2[1] = y1[1] + (float)item2.getHeight();
+
+        item3.getLocationOnScreen(pos);
+        x1[2] = (float)pos[0];
+        y1[2] = (float)pos[1];
+        x2[2] = x1[2] + (float)item3.getWidth();
+        y2[2] = y1[2] + (float)item3.getHeight();
+
+        item4.getLocationOnScreen(pos);
+        x1[3] = (float)pos[0];
+        y1[3] = (float)pos[1];
+        x2[3] = x1[3] + (float)item4.getWidth();
+        y2[3] = y1[3] + (float)item4.getHeight();
+
+        numb1.getLocationOnScreen(pos);
+        x1[0] = (float)pos[0];
+        y1[0] = (float)pos[1];
+        x2[0] = x1[0] + (float)numb1.getWidth();
+        y2[0] = y1[0] + (float)numb1.getHeight();
+
+        numb2.getLocationOnScreen(pos);
+        x1[1] = (float)pos[0];
+        y1[1] = (float)pos[1];
+        x2[1] = x1[1] + (float)numb2.getWidth();
+        y2[1] = y1[1] + (float)numb2.getHeight();
+
+        numb3.getLocationOnScreen(pos);
+        x1[2] = (float)pos[0];
+        y1[2] = (float)pos[1];
+        x2[2] = x1[2] + (float)numb3.getWidth();
+        y2[2] = y1[2] + (float)numb3.getHeight();
+
+        numb4.getLocationOnScreen(pos);
+        x1[3] = (float)pos[0];
+        y1[3] = (float)pos[1];
+        x2[3] = x1[3] + (float)numb4.getWidth();
+        y2[3] = y1[3] + (float)numb4.getHeight();
 
         width = getWindowManager().getDefaultDisplay().getWidth();
         height = getWindowManager().getDefaultDisplay().getHeight();
